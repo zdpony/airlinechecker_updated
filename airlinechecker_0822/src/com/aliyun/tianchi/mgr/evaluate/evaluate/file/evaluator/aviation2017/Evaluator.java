a60ab51b9e11339044b185b60c805e580af1e4e6
zdpony <zd.pony@gmail.com>
2017-08-23 14:51:30 +0800

    new project

---
+++ airlinechecker_0822/src/com/aliyun/tianchi/mgr/evaluate/evaluate/file/evaluator/aviation2017/Evaluator.java
@@ -0,0 +1,29 @@
+package com.aliyun.tianchi.mgr.evaluate.evaluate.file.evaluator.aviation2017;
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * Created by peicheng on 17/6/28.
+ */
+public class Evaluator {
+
+    public static void main(String[] args) {
+    	 String inputDataFilePath = "data/厦航大赛数据20170814.xlsx";
+         //String resultDataFilePath = "data/baseline_result_2.csv";
+         String resultDataFilePath = "data/firstresult821.csv";
+         
+        try {
+            //计算所得分数
+            InputStream inputDataStream = new FileInputStream(inputDataFilePath);
+            InputStream resultDataStream = new FileInputStream(resultDataFilePath);
+            ResultEvaluator resultEvaluator = new ResultEvaluator(inputDataStream);
+            double score = resultEvaluator.runEvaluation(resultDataStream);
+            System.out.println("选手所得分数为：" + score);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
